77c8d460a7d4e90e7fd68f8437bc918a8efd663d
Arunesh2004 <sharmaarunesh07@gmail.com>
2024-09-19 19:48:50 +0530

    Third Commit

---
+++ src/Arrays/CreatingArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class CreatingArray {
+
+    public static void main(String[] args) {
+
+        //Syntax;
+
+        // datatype[] variablename = new datatype[size];
+
+              //OR
+
+        // datatype[] variablename = {elements};
+
+        int[] rollno = new int[5];
+
+                   //OR
+
+        int[] rollno2 = {5 , 8 , 9 , 89 , 67};
+    }
+}
