ced2ef8af0ee9e0e59a708569ff8e41c2dd931c7
Arunesh2004 <sharmaarunesh07@gmail.com>
2024-09-28 18:56:59 +0530

    Fifth Commit

---
+++ src/LinearSearching/ReturnElement.java
@@ -0,0 +1,34 @@
+package LinearSearching;
+
+import java.util.Scanner;
+
+public class ReturnElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc =  new Scanner(System.in);
+
+        int[] arr = {45 , 54 , 32 , 34 , 23};
+
+        int target = sc.nextInt();
+
+        int res = LinearSearch(arr , target);
+
+        System.out.println(res);
+
+    }
+
+
+    static int LinearSearch(int[] arr , int target){
+
+        for(int value : arr){
+
+            if(value == target){
+
+                return value;
+            }
+        }
+
+        return -1;
+    }
+}
